01957fb33c3ed483032b5c5534049a1a5870cfd0
conrad <conrad0803cool@gmail.com>
2020-02-26 18:46:07 -0600

    Updates

---
+++ precache-manifest.1b395870db360acff3b9a91016221611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7cf6df25f92b114eca0b17b53af24fc",
+    "url": "/Sorting-Shuffle-Visualizer/index.html"
+  },
+  {
+    "revision": "f4b61a28c8d7d6bfbcf5",
+    "url": "/Sorting-Shuffle-Visualizer/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "31014ad4e8053e5d2ec2",
+    "url": "/Sorting-Shuffle-Visualizer/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "f4b61a28c8d7d6bfbcf5",
+    "url": "/Sorting-Shuffle-Visualizer/static/js/2.cff339fb.chunk.js"
+  },
+  {
+    "revision": "31014ad4e8053e5d2ec2",
+    "url": "/Sorting-Shuffle-Visualizer/static/js/main.81a347e7.chunk.js"
+  },
+  {
+    "revision": "f65f7d9684cf4958ea65",
+    "url": "/Sorting-Shuffle-Visualizer/static/js/runtime-main.b8ddc3ce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28ca76f509c30c0bac6dc52dc43a7d09.js"
+  "/Sorting-Shuffle-Visualizer/precache-manifest.1b395870db360acff3b9a91016221611.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sorting-Shuffle-Visualizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
